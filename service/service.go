6107fe638c97a3f1924cd9a4fcdb63497d9e2d53
milossimic <milossimicsimo@gmail.com>
2019-11-18 23:16:30 +0100

    extended blackole to call apollo service in order to check does user have rights

--- service/service.go
+++ service/service.go
@@ -5,15 +5,18 @@ import (
 	"github.com/c12s/blackhole/model"
 	"github.com/c12s/blackhole/queue"
 	storage "github.com/c12s/blackhole/storage"
+	aPb "github.com/c12s/scheme/apollo"
 	pb "github.com/c12s/scheme/blackhole"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"time"
 )
 
 type Server struct {
-	Queue *queue.BlackHole
+	Queue  *queue.BlackHole
+	Apollo string
 }
 
 func (s *Server) getTK(ctx context.Context, req *pb.PutReq) (*queue.TaskQueue, error) {
@@ -32,6 +35,19 @@ func (s *Server) getTK(ctx context.Context, req *pb.PutReq) (*queue.TaskQueue, e
 }
 
 func (s *Server) Put(ctx context.Context, req *pb.PutReq) (*pb.Resp, error) {
+	client := NewApolloClient(s.Apollo)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	resp, err := client.Auth(ctx, &aPb.AuthOpt{})
+	if err != nil {
+		return &pb.Resp{Msg: err.Error()}, nil
+	}
+
+	if !resp.Value {
+		return &pb.Resp{Msg: "You do not have access for this service"}, nil
+	}
+
 	tk, err := s.getTK(ctx, req)
 	if err != nil {
 		return nil, err
@@ -46,7 +62,7 @@ func (s *Server) Put(ctx context.Context, req *pb.PutReq) (*pb.Resp, error) {
 	return &pb.Resp{Msg: pResp.Msg}, nil
 }
 
-func Run(ctx context.Context, db storage.DB, address, celestial string, opts []*model.TaskOption) {
+func Run(ctx context.Context, db storage.DB, address, celestial, apollo string, opts []*model.TaskOption) {
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to initializa TCP listen: %v", err)
@@ -55,7 +71,8 @@ func Run(ctx context.Context, db storage.DB, address, celestial string, opts []*
 
 	server := grpc.NewServer()
 	blackholeServer := &Server{
-		Queue: queue.New(ctx, db, opts, celestial),
+		Queue:  queue.New(ctx, db, opts, celestial),
+		Apollo: apollo,
 	}
 
 	fmt.Println("BlackHoleService RPC Started")
